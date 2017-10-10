b7768d0b4ca71e4acd94032181de423c71915c76
mdbyars <mdbyars@haverford.edu>
2017-10-10 16:30:19 -0400

    locking in changes before switching computesr

---
+++ preprocessing/PreprocessHelpers/GermanProcessing.py
@@ -0,0 +1,160 @@
+import numpy as np
+sys.path.append('/home/h205c/jnim/fairness/fairness-comparison/data')
+import pandas as pd
+
+
+"""
+0 = woman
+1 = man
+1 = good credit
+0 = bad credit
+"""
+#Evan says that he set the weird male/female category to just male/female, scaled numerical values to be between 0 and 1, and removed unordered categorical features, and one-hot encoded ordered categorical data. 
+
+def prepare_german():
+	X = []
+	y = []
+	x_control = []
+	for line in open(str(filename)):
+		line = line.strip()
+		if line == "": continue # skip+ empty lines
+
+		if line[0] == "s": continue # skip line of feature categories, in csv
+		line = line.split(",")
+
+		"""
+		Get class label
+		"""
+		class_label = line[-1]
+		y.append(class_label)
+
+
+		"""
+		Repair sex variable
+		"""
+
+		if line[8] == "A95" or line[8] == "A92":
+			line[8] = 0
+		else:
+			line[8] = 1
+
+
+		line = line[:8]+line[9:-1]
+		x_val = []
+
+		"""
+		Hot encode categorical data
+		"""
+		for i in range(0, len(line)):
+			
+			#I want x_val to be extended by each value in i after hot-encoding - set x_val = pd.get_dummies(protected)
+            #Attribute 1
+			if i == 0:
+				if line[i] == "A11":
+					x_val.extend([1, 0, 0, 0])
+				if line[i] == "A12":
+					x_val.extend([0, 1, 0, 0])
+				if line[i] == "A13":
+					x_val.extend([0, 0, 1, 0])
+				if line[i] == "A14":
+					x_val.extend([0, 0, 0, 1])
+
+            #Attribute 2
+			if i == 1:
+				x_val.append(line[i])
+
+			#Attribute 3
+			if i == 2:
+				if line[i] == "A30":
+					x_val.extend([1, 0, 0, 0, 0])
+				if line[i] == "A31":
+					x_val.extend([0, 1, 0, 0, 0])
+				if line[i] == "A32":
+					x_val.extend([0, 0, 1, 0, 0])
+				if line[i] == "A33":
+					x_val.extend([0, 0, 0, 1, 0])
+				if line[i] == "A34":
+					x_val.extend([0, 0, 0, 0, 1])
+	
+			#Attribute 5
+			if i == 4:
+				x_val.append(line[i])
+	
+			#Attribute 6
+			if i == 5:
+				if line[i] == "A61":
+					x_val.extend([1, 0, 0, 0, 0])
+				if line[i] == "A62":
+					x_val.extend([0, 1, 0, 0, 0])
+				if line[i] == "A63":
+					x_val.extend([0, 0, 1, 0, 0])
+				if line[i] == "A64":
+					x_val.extend([0, 0, 0, 1, 0])
+				if line[i] == "A65":
+					x_val.extend([0, 0, 0, 0, 1])
+	
+			#Attribute 7
+			if i == 6:
+				if line[i] == "A71":
+					x_val.extend([1, 0, 0, 0, 0])
+				if line[i] == "A72":
+					x_val.extend([0, 1, 0, 0, 0])
+				if line[i] == "A73":
+					x_val.extend([0, 0, 1, 0, 0])
+				if line[i] == "A74":
+					x_val.extend([0, 0, 0, 1, 0])
+				if line[i] == "A75":
+					x_val.extend([0, 0, 0, 0, 1])
+	
+			#Attribute 8
+			if i == 7:
+				x_val.append(line[i])
+	
+			#Attribute 10
+			if i == 8:
+				if line[i] == "A101":
+					x_val.extend([1, 0, 0])
+				if line[i] == "A102":
+					x_val.extend([0, 1, 0])
+				if line[i] == "A103":
+					x_val.extend([0, 0, 1])
+	
+			#Attribute 13
+			if i == 11:
+				x_val.append(line[i])
+	
+			#Attributr 16
+			if i == 14:
+				x_val.append(line[i])
+	
+			#Attribute 17
+			if i == 15:
+				if line[i] == "A171":
+					x_val.extend([1, 0, 0, 0])
+				if line[i] == "A172":
+					x_val.extend([0, 1, 0, 0])
+				if line[i] == "A173":
+					x_val.extend([0, 0, 1, 0])
+				if line[i] == "A174":
+					x_val.extend([0, 0, 0, 1])
+	
+			#Attribute 18
+			if i == 16:
+				x_val.append(line[i])
+
+
+		X.append(x_val)
+
+	x_control = {"sex": x_control}
+	X = np.array(X)
+	X = X.astype(float)
+	y = np.array(y)
+	y = y.astype(float)
+
+
+	return X, y, x_control
+	print(X)
+
+if __name__ == '__main__':
+
+print(load_german_data("german_credit_data.csv"))
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.append('/home/h205c/jnim/fairness/fairness-comparison/data')
+from PreprocessHelpers.GermanProcessing import *
 import pandas as pd
 
 def prepareData(dataset):
-	#path = '../data/'
+	path = '../data/'
 	if dataset == 'german':
 		
-		german_data = pd.read_csv(sys.path[0] + '/raw/german/german_data', sep = ' ', encoding = 'ISO-8859-1')
+		german_data = pd.read_csv(path + '/raw/german/german_data', sep = ' ', encoding = 'ISO-8859-1')
 		
 		#It's time to hard-code in the rows
 		german_data.columns = ['status', 'month', 'credit_history', 'purpose', 'credit_amount', \
@@ -15,12 +15,16 @@ def prepareData(dataset):
 		'number_of_credits', 'skill_level', 'people_liable_for', 'telephone', 'foreign_worker', \
 		'credit']
 		
-		german_data.to_csv(path + '/preprocessed/german_credit_data.csv', index = False)
+		german_data.to_csv(path + '/preprocessed/german_credit_data.csv', index = False) #Save the CSV because it gets used here and there
+		
+		german_data_numeric = prepare_german('numeric') #GermanProcessing from PreprocessHelpers, currently sex is forced variable
 		
 		print(len(german_data))
 		
 	if dataset == 'adult': 
-		adult_data = pd.read_csv(path + 'adult/adult.data', sep = ' ')
+		adult_data = pd.read_csv(path + 'adult/adult.data', sep = ' ', encoding = 'ISO-8859-1')
+		
+		
 		
 		
 if __name__ == '__main__': 
