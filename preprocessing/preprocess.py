80386f5db988f11715ba016ac9bfeae62d3add41
mdbyars <mdbyars@haverford.edu>
2017-10-10 15:06:27 -0400

    adding raw data to correct place

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,11 +1,12 @@
 import sys
 sys.path.append('/home/h205c/jnim/fairness/fairness-comparison/data')
 import pandas as pd
-def prepareData():
-	path = '../data/'
+
+def prepareData(dataset):
+	#path = '../data/'
 	if dataset == 'german':
 		
-		german_data = pd.read_csv(path + 'german/german_data', sep = ' ', encoding = 'ISO-8859-1')
+		german_data = pd.read_csv(sys.path[0] + '/raw/german/german_data', sep = ' ', encoding = 'ISO-8859-1')
 		
 		#It's time to hard-code in the rows
 		german_data.columns = ['status', 'month', 'credit_history', 'purpose', 'credit_amount', \
@@ -16,11 +17,11 @@ def prepareData():
 		
 		german_data.to_csv(path + '/preprocessed/german_credit_data.csv', index = False)
 		
-		print(len(german))
+		print(len(german_data))
 		
 	if dataset == 'adult': 
-		adult_data = pd.read_csv(path + 'adult/adult.data', sep
+		adult_data = pd.read_csv(path + 'adult/adult.data', sep = ' ')
 		
 		
 if __name__ == '__main__': 
-	prepareData()
+	prepareData('german')
