d9438087b21982621d12cf4dc2af9db19ff3dd56
Sorelle Friedler <sorelle@cs.haverford.edu>
2017-12-21 11:33:42 -0500

    doing some work on new_benchmarks

--- new_benchmarks.py
+++ new_benchmarks.py
@@ -6,6 +6,7 @@ from algorithms.list import ALGORITHMS
 from metrics.list import METRICS
 
 NUM_TRIALS_DEFAULT = 10
+RESULT_DIR = "results/"
 
 def run(num_trials = NUM_TRIALS_DEFAULT, dataset_names = get_dataset_names()):
     for dataset in DATASETS:
@@ -13,14 +14,19 @@ def run(num_trials = NUM_TRIALS_DEFAULT, dataset_names = get_dataset_names()):
             continue
         processed_dataset = ProcessedData(dataset)
         processed_splits, numerical_splits = processed_dataset.create_train_test_splits(num_trials)
+
         for algorithm in ALGORITHMS:
             for i in range(0, num_trials):
                 train, test = processed_splits[i]
-                algorithm.run(train, test, dataset.get_sensitive_attributes(), None)
+                actual, predicted, sensitive = algorithm.run(train, test, 
+                                                             dataset.get_sensitive_attributes(), None)
 
                 for metric in METRICS:
-                    # write metric, algorithm to dataset file
-                    pass
+                    m = Metric(actual, predicted)
+                    result = m.calc() 
+                    ## TODO: save metric so that avg and stddev can be calculated
+            ## TODO: calculate avg and stddev per metric
+            ## TODO: write avg and stddev per metric to file
 
 def main():
     fire.Fire(run)
