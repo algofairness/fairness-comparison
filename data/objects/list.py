52794be87332d2ddd1b012bf2eb373d3ecc6248f
Sorelle Friedler <sorelle@cs.haverford.edu>
2018-01-16 12:33:13 -0500

    adding propublica violent recidivism data

--- data/objects/list.py
+++ data/objects/list.py
@@ -2,8 +2,9 @@ from data.objects.Ricci import Ricci
 from data.objects.Adult import Adult
 from data.objects.German import German
 from data.objects.PropublicaRecidivism import PropublicaRecidivism
+from data.objects.PropublicaViolentRecidivism import PropublicaViolentRecidivism
 
-DATASETS = [ Ricci(), Adult(), German(), PropublicaRecidivism() ]
+DATASETS = [ Ricci(), Adult(), German(), PropublicaRecidivism(), PropublicaViolentRecidivism() ]
 
 def get_dataset_names():
     names = []
