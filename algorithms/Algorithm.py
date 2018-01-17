b4b9c021e17d1500de09cb55ecfff893a262fd65
Sorelle Friedler <sorelle@cs.haverford.edu>
2018-01-17 13:29:42 -0500

    adding comments to Algorithm

--- algorithms/Algorithm.py
+++ algorithms/Algorithm.py
@@ -1,18 +1,28 @@
 
 class Algorithm():
+    """
+    This is the base class for all implemented algorithms.  New algorithms should extend this
+    class, implement run below, and set self.name in the init method.  Other optional methods to
+    implement are described below.
+    """
+
     def __init__(self):
         pass
 
-    def run(self, train_df, test_df, class_attr, positive_class_val, sensitive_attrs, 
+    def run(self, train_df, test_df, class_attr, positive_class_val, sensitive_attrs,
             single_sensitive, privileged_vals, params):
         """
-        Runs the algorithm and returns the predicted classifications on the test set.  The given 
+        Runs the algorithm and returns the predicted classifications on the test set.  The given
         train and test data still contains the sensitive_attrs.  This run of the algorithm
         should focus on the single given sensitive attribute.
 
         params: a dictionary mapping from algorithm-specific parameter names to the desired values.
         If the implementation of run uses different values, these should be modified in the params
         dictionary as a way of returning the used values to the caller.
+
+        Be sure that the returned predicted classifications are of the same type as the class
+        attribute in the given test_df.  If this is not the case, some metric analyses may fail to
+        appropriately compare the returned predictions to their desired values.
 
         TODO: figure out how to indicate that an algorithm that can handle multiple sensitive
         attributes should do so now.
@@ -35,7 +45,11 @@ class Algorithm():
 
     def handles_multiple_sensitive_attrs(self):
         """
-        Returns True if this algorithm can handle multiple sensitive attributes.
+        Returns True if this algorithm can handle multiple sensitive attributes - i.e., can repair
+        for both race and sex at the same time.  All algorithms will be tested using the joint
+        distribution of multiple sensitive attributes as if they were a single attribute -
+        algorithms should only return True here if they have a unique solution beyond this for
+        handling multiple sensitive attributes.
         """
         return False
 
@@ -46,6 +60,12 @@ class Algorithm():
         return False
 
     def get_name(self):
+        """
+        Returns the name for the algorithm.  This must be a unique name, so it is suggested that
+        this name is simply <firstauthor>.  If there are mutliple algorithms by the same author(s), a
+        suggested modification is <firstauthor-algname>.  This name will appear in the resulting
+        CSVs and graphs created when performing benchmarks and analysis.
+        """
         return self.name
 
     def get_default_params(self):
