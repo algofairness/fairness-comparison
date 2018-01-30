ddd3903bd6c8aca6f616357bb1492dac60db6c60
Sonam Choudhary <31292156+SonamChoudhary@users.noreply.github.com>
2018-01-29 21:54:54 -0700

    Update list.py

--- metrics/list.py
+++ metrics/list.py
@@ -5,4 +5,4 @@ from metrics.MCC import MCC
 from metrics.TNR import TNR
 from metrics.TPR import TPR
 
-METRICS = [ Accuracy(), TPR(), TNR(), MCC(), DisparateImpact(), CV() ]
+METRICS = [ Accuracy(), TPR(), TNR(), MCC(), DisparateImpact(), CV(), EqOppo_fp_diff(), EqOppo_fp_ratio(), EqOppo_fn_diff(), EqOppo_fn_ratio()]
