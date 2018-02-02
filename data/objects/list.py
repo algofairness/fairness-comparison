bb3a0a9989790d1fda55645e62bcbcec9e11e73d
Carlos Scheidegger <cscheid@cs.arizona.edu>
2018-02-02 10:09:45 -0700

    very basic two gaussians synthetic dataset

--- data/objects/list.py
+++ data/objects/list.py
@@ -4,8 +4,16 @@ from data.objects.Adult import Adult
 from data.objects.German import German
 from data.objects.PropublicaRecidivism import PropublicaRecidivism
 from data.objects.PropublicaViolentRecidivism import PropublicaViolentRecidivism
+from data.objects.TwoGaussians import TwoGaussians
 
-DATASETS = [ Ricci(), Adult(), German(), PropublicaRecidivism(), PropublicaViolentRecidivism() ]
+DATASETS = [
+    Ricci(),
+    Adult(),
+    German(),
+    PropublicaRecidivism(),
+    PropublicaViolentRecidivism(),
+    TwoGaussians()
+    ]
 
 # For testing, you can just use a sample of the data.  E.g.:
 # DATASETS = [ Sample(Adult(), 50) ]
